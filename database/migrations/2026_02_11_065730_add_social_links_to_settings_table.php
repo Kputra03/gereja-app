81ac55d29b14b9f24a4f050a9300f1044f65c947
Kputra03 <natanaelkevin9@gmail.com>
2026-03-12 13:14:32 +0700

    fix duplicate instagram column

--- database/migrations/2026_02_11_065730_add_social_links_to_settings_table.php
+++ database/migrations/2026_02_11_065730_add_social_links_to_settings_table.php
@@ -6,27 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
-{
-    Schema::table('settings', function (Blueprint $table) {
-        $table->string('instagram')->nullable();
-        $table->string('instagram_pemuda')->nullable();
-        $table->string('instagram_anak')->nullable();
-    });
-}
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->string('instagram_pemuda')->nullable();
+            $table->string('instagram_anak')->nullable();
+        });
+    }
 
-
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('settings', function (Blueprint $table) {
-            //
+            $table->dropColumn([
+                'instagram_pemuda',
+                'instagram_anak'
+            ]);
         });
     }
 };
